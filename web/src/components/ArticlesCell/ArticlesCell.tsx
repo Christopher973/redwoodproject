16b7a9c58121e83e4fabb4cda78f51a9082c88a4
Christopher Marie-Angélique <christopher.marieangelique.pro@gmail.com>
2024-10-23 15:32:03 +0200

    save

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,3 +1,7 @@
+import { Link, routes } from '@redwoodjs/router'
+
+import Article from 'src/components/Article'
+
 export const QUERY = gql`
   query ArticlesQuery {
     articles: posts {
@@ -21,13 +25,7 @@ export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <>
       {articles.map((article) => (
-        <article key={article.id}>
-          <header>
-            <h2>{article.title}</h2>
-          </header>
-          <p>{article.body}</p>
-          <div>Posted at: {article.createdAt}</div>
-        </article>
+        <Article key={article.id} article={article} />
       ))}
     </>
   )
